bdd0558f6c39e15fe31b644535329db5e4085ed6
William C. Webb <williamwebb35@gmail.com>
2021-04-24 18:36:04 -0700

    adding numberOfSteps.py

---
+++ numberOfSteps.py
@@ -0,0 +1,18 @@
+"""
+Given a non-negative integer num, return the number of steps to
+reduce it to zero. If the current number is even, you have to
+divide it by 2, otherwise, you have to subtract 1 from it.
+"""
+
+
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        steps = 0
+        while num > 0:
+            if num % 2 == 0:
+                num = num/2
+                steps += 1
+            else:
+                num = num -1
+                steps +=1
+        return(steps)    
